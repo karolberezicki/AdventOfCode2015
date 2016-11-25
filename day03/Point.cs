434aa72099c60f5b434ac22807a9dbfb2989bcc6
Karol Berezicki <karolberezicki@gmail.com>
2016-11-25 23:13:28 +0100

    Day03

---
+++ day03/Point.cs
@@ -0,0 +1,59 @@
+﻿namespace day03
+{
+
+    public class Point
+    {
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public Point(Point point)
+        {
+            X = point.X;
+            Y = point.Y;
+        }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public void Move(char direction)
+        {
+            switch (direction)
+            {
+                case '^':
+                    X++;
+                    break;
+                case 'v':
+                    X--;
+                    break;
+                case '>':
+                    Y++;
+                    break;
+                case '<':
+                    Y--;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            int hash = 23;
+            hash = hash * 31 + X.GetHashCode();
+            hash = hash * 31 + Y.GetHashCode();
+            return hash;
+        }
+
+        public override bool Equals(object obj)
+        {
+            Point point = obj as Point;
+            if (point == null) return false;
+            return (X == point.X && Y == point.Y);
+        }
+
+
+    }
+}
---
+++ day03/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace day03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string source = File.ReadAllText(@"..\..\input.txt");
+
+            // Part 1
+            Point currentSantaLocation = new Point(0, 0);
+            List<Point> visitedSantaHouses = new List<Point>();
+            visitedSantaHouses.Add(new Point(currentSantaLocation));
+
+            foreach (char direction in source.ToCharArray())
+            {
+                currentSantaLocation.Move(direction);
+                visitedSantaHouses.Add(new Point(currentSantaLocation));
+            }
+
+            List<IGrouping<Point, Point>> uniqueHouses =
+                visitedSantaHouses.GroupBy(d => d).Select(d => d).ToList();
+            Console.WriteLine("Unique visted houses by Santa count = {0}", uniqueHouses.Count);
+
+
+            // Part 2
+            currentSantaLocation = new Point(0, 0);
+            visitedSantaHouses = new List<Point>();
+            visitedSantaHouses.Add(new Point(currentSantaLocation));
+
+            Point currentRoboSantaLocation = new Point(0, 0);
+            List<Point> visitedRoboSantaHouses = new List<Point>();
+            visitedRoboSantaHouses.Add(new Point(currentRoboSantaLocation));
+
+            bool isSantasTurn = true;
+
+            foreach (char direction in source.ToCharArray())
+            {
+                if (isSantasTurn)
+                {
+                    currentSantaLocation.Move(direction);
+                    visitedSantaHouses.Add(new Point(currentSantaLocation));
+                }
+                else
+                {
+                    currentRoboSantaLocation.Move(direction);
+                    visitedRoboSantaHouses.Add(new Point(currentRoboSantaLocation));
+                }
+                isSantasTurn = !isSantasTurn;
+            }
+
+            uniqueHouses = visitedSantaHouses.Concat(visitedRoboSantaHouses)
+                .GroupBy(d => d).Select(d => d).ToList();
+
+            Console.WriteLine("Unique visted houses by Santa and RoboSanta count = {0}", uniqueHouses.Count);
+
+            Console.ReadLine();
+
+        }
+    }
+}
