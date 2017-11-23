538aa1c9b53ea99fa929e0ad18d799beb0181880
Karol Berezicki <karolberezicki@gmail.com>
2017-11-23 22:02:16 +0100

    Day15, added parsing of input

---
+++ day15/Ingredient.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace day15
+{
+    [DebuggerDisplay("{Name}: Capacity {Capacity}, Durability {Durability}, Flavor {Flavor}, Texture {Texture}, Calories {Calories}")]
+    public class Ingredient
+    {
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public int Durability { get; set; }
+        public int Flavor { get; set; }
+        public int Texture { get; set; }
+        public int Calories { get; set; }
+
+
+        public Ingredient(string instruction)
+        {
+            const string pattern = @"(\w+)\: capacity ([-+]?[0-9]+), durability ([-+]?[0-9]+), flavor ([-+]?[0-9]+), texture ([-+]?[0-9]+), calories ([-+]?[0-9]+)";
+            Regex regex = new Regex(pattern);
+            Match match = regex.Match(instruction);
+
+            Name = match.Groups[1].Value;
+            Capacity = int.Parse(match.Groups[2].Value);
+            Durability = int.Parse(match.Groups[3].Value);
+            Flavor = int.Parse(match.Groups[4].Value);
+            Texture = int.Parse(match.Groups[5].Value);
+            Calories = int.Parse(match.Groups[6].Value);
+        }
+    }
+}
---
+++ day15/Program15.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace day15
+{
+    public class Program15
+    {
+        public static void Main()
+        {
+            string source = File.ReadAllText(@"..\..\input.txt");
+            List<string> instructions = source.Split('\n').ToList();
+
+            var ingridients = instructions.Select(i => new Ingredient(i)).ToList();
+
+            Console.ReadKey();
+        }
+    }
+}
