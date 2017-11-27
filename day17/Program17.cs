f68984c6f88142016df021099c3bd04d2204ba7e
Karol Berezicki <karolberezicki@gmail.com>
2017-11-27 20:05:27 +0100

    Day17, parse input

---
+++ day17/Program17.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day17
+{
+    public class Program17
+    {
+        public static void Main()
+        {
+            string source = File.ReadAllText(@"..\..\input.txt");
+            source = source.Remove(source.Length - 1);
+            List<int> containers = source.Split('\n').Select(c => int.Parse(c)).ToList();
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
