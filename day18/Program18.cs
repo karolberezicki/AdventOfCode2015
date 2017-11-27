77cefbac616057b0404d96786f27295a56eb0267
Karol Berezicki <karolberezicki@gmail.com>
2017-11-27 22:08:54 +0100

    Day 18, added input parse

--- day18/Program18.cs
+++ day18/Program18.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,19 @@ namespace day18
     {
         public static void Main()
         {
+
+            string source = File.ReadAllText(@"..\..\input.txt");
+            source = source.Remove(source.Length - 1);
+            var input = source.Split('\n').Select(c => c.Select(b => b == '#').ToArray()).ToArray();
+
+            for (int i = 0; i < 100; i++)
+            {
+                for (int j = 0; j < 100; j++)
+                {
+
+                }
+            }
+
         }
     }
 }
