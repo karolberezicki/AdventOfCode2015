f3fb7f02aa8654d8ffbf81c44001394be205fdb2
Karol Berezicki <karolberezicki@gmail.com>
2017-11-27 19:45:27 +0100

    Day16, part one and two

---
+++ day16/AuntSue.cs
@@ -0,0 +1,72 @@
+﻿namespace day16
+{
+    public partial class Program15
+    {
+        public class AuntSue
+        {
+            public AuntSue(string i)
+            {
+                i = i.Replace("Sue", "").Replace(" ", "");
+                Number = int.Parse(i.Substring(0, i.IndexOf(':')));
+                var compounds = i.Substring(i.IndexOf(':') + 1).Split(',');
+
+                foreach (string compound in compounds)
+                {
+                    SetCompounds(compound);
+                }
+            }
+
+            public int Number { get; set; }
+
+            public int? children { get; set; }
+            public int? cats { get; set; }
+            public int? samoyeds { get; set; }
+            public int? pomeranians { get; set; }
+            public int? akitas { get; set; }
+            public int? vizslas { get; set; }
+            public int? goldfish { get; set; }
+            public int? trees { get; set; }
+            public int? cars { get; set; }
+            public int? perfumes { get; set; }
+
+            private void SetCompounds(string compound)
+            {
+                var compoundSplited = compound.Split(':');
+                var compoundValue = int.Parse(compoundSplited[1]);
+                switch (compoundSplited[0])
+                {
+                    case nameof(children):
+                        children = compoundValue;
+                        break;
+                    case nameof(cats):
+                        cats = compoundValue;
+                        break;
+                    case nameof(samoyeds):
+                        samoyeds = compoundValue;
+                        break;
+                    case nameof(pomeranians):
+                        pomeranians = compoundValue;
+                        break;
+                    case nameof(akitas):
+                        akitas = compoundValue;
+                        break;
+                    case nameof(vizslas):
+                        vizslas = compoundValue;
+                        break;
+                    case nameof(goldfish):
+                        goldfish = compoundValue;
+                        break;
+                    case nameof(trees):
+                        trees = compoundValue;
+                        break;
+                    case nameof(cars):
+                        cars = compoundValue;
+                        break;
+                    case nameof(perfumes):
+                        perfumes = compoundValue;
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ day16/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace day16
+{
+    public partial class Program15
+    {
+        public static void Main()
+        {
+            string source = File.ReadAllText(@"..\..\input.txt");
+            List<string> instructions = source.Split('\n').Take(500).ToList();
+
+            List<AuntSue> auntSues = instructions.Select(i => new AuntSue(i)).ToList();
+            int auntForPartOne = FindAuntForPartOne(auntSues);
+            int auntForPartTwo = FindAuntForPartTwo(auntSues);
+
+            Console.WriteLine($"Aunt Sue - part one number : {auntForPartOne}");
+            Console.WriteLine($"Aunt Sue - part two number : {auntForPartTwo}");
+
+            Console.ReadKey();
+        }
+
+        private static int FindAuntForPartOne(IEnumerable<AuntSue> auntSues)
+        {
+            return auntSues.First(a => (a.children == 3 || a.children == null) &&
+                                       (a.cats == 7 || a.cats == null) &&
+                                       (a.samoyeds == 2 || a.samoyeds == null) &&
+                                       (a.pomeranians == 3 || a.pomeranians == null) &&
+                                       (a.akitas == 0 || a.akitas == null) &&
+                                       (a.vizslas == 0 || a.vizslas == null) &&
+                                       (a.goldfish == 5 || a.goldfish == null) &&
+                                       (a.trees == 3 || a.trees == null) &&
+                                       (a.cars == 2 || a.cars == null) &&
+                                       (a.perfumes == 1 || a.perfumes == null)).Number;
+        }
+
+        private static int FindAuntForPartTwo(IEnumerable<AuntSue> auntSues)
+        {
+            return auntSues.First(a => (a.children == 3 || a.children == null) &&
+                                       (a.cats > 7 || a.cats == null) &&
+                                       (a.samoyeds == 2 || a.samoyeds == null) &&
+                                       (a.pomeranians < 3 || a.pomeranians == null) &&
+                                       (a.akitas == 0 || a.akitas == null) &&
+                                       (a.vizslas == 0 || a.vizslas == null) &&
+                                       (a.goldfish < 5 || a.goldfish == null) &&
+                                       (a.trees > 3 || a.trees == null) &&
+                                       (a.cars == 2 || a.cars == null) &&
+                                       (a.perfumes == 1 || a.perfumes == null)).Number;
+        }
+    }
+}
