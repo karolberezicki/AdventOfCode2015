e4cca71e5f5ab6d0f2c561d09b949664d8f9409b
Karol Berezicki <karolberezicki@gmail.com>
2017-12-05 19:03:35 +0100

    Day20, part one

---
+++ day20/Program20.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day20
+{
+    public class Program20
+    {
+        public static void Main(string[] args)
+        {
+            var input = 34000000;
+
+            int partOne = PartOne(input);
+        }
+
+
+        public static int PartOne(int input)
+        {
+            for (int houseNumber = 1; ; houseNumber++)
+            {
+                int housePresents = Factor(houseNumber).Sum() * 10;
+
+                if (housePresents >= input)
+                {
+                    return houseNumber;
+                }
+            }
+        }
+
+        // Credit to: https://stackoverflow.com/a/239877/7454424
+        public static List<int> Factor(int number)
+        {
+            List<int> factors = new List<int>();
+            int max = (int)Math.Sqrt(number);  //round down
+            for (int factor = 1; factor <= max; ++factor)
+            { //test from 1 to the square root, or the int below it, inclusive.
+                if (number % factor == 0)
+                {
+                    factors.Add(factor);
+                    if (factor != number / factor)
+                    { // Don't add the square root twice!  Thanks Jon
+                        factors.Add(number / factor);
+                    }
+                }
+            }
+
+            return factors;
+        }
+
+    }
+
+}
